39ddbeb275150a00fa1d8a4bc36354c7d2bcc2d5
mohammadjalizi <mjalizi1376@gmail.com>
2023-04-08 16:47:03 +0330

    creact  store js

--- front-end/src/Redux/ProudoctsApi.js
+++ front-end/src/Redux/ProudoctsApi.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 // Define a service using a base URL and expected endpoints
-export const Proudocts = createApi({
+export const ProudoctsAPI = createApi({
   reducerPath: 'Proudocts',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/' }),
   endpoints: (builder) => ({
@@ -13,4 +13,4 @@ export const Proudocts = createApi({
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
-export const { useGetPokemonByNameQuery } = Proudocts
+export const { useGetPokemonByNameQuery } = ProudoctsAPI
