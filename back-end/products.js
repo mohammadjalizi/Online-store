2edf22074a459903836258a75393177291079cbe
mohammadjalizi <mjalizi1376@gmail.com>
2023-04-08 16:13:08 +0330

    creact products

---
+++ back-end/products.js
@@ -0,0 +1,36 @@
+const products = [
+    {
+      id: 1,
+      productName: "T-shirt",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+      price: 100,
+      imageLink: "https://res.cloudinary.com/dlz1h6dhr/image/upload/v1680872470/online%20store/1_mrh258.jpg",
+    },
+    {
+      id: 2,
+      productName: "T-shirt",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+      price: 200,
+      imageLink: "https://res.cloudinary.com/dlz1h6dhr/image/upload/v1680872470/online%20store/4_suniet.jpg",
+    },
+    {
+      id: 3,
+      productName: "T-shirt",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+      price: 300,
+      imageLink: "https://res.cloudinary.com/dlz1h6dhr/image/upload/v1680872470/online%20store/3_stpazu.jpg",
+    },
+    {
+      id: 4,
+      productName: "T-shirt",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+      price: 400,
+      imageLink: "https://res.cloudinary.com/dlz1h6dhr/image/upload/v1680872470/online%20store/2_w4m0zr.jpg",
+    },
+   ];
+   module.exports = products;
+   
