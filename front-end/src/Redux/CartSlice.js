c4c355daf5bb6d2132104d821d2319bd2a75223d
mohammadjalizi <mjalizi1376@gmail.com>
2023-04-11 15:52:55 +0330

    Using Redux Toolkit in our project

--- front-end/src/Redux/CartSlice.js
+++ front-end/src/Redux/CartSlice.js
@@ -1,23 +1,15 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-  value: 0,
+  selectedProducts: [],
 }
 
 export const counterSlice = createSlice({
-  name: 'counter',
+  name: 'cart',
   initialState,
   reducers: {
-    increment: (state) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
-      state.value += 1
-    },
-    decrement: (state) => {
-      state.value -= 1
-    },
+
+  
     incrementByAmount: (state, action) => {
       state.value += action.payload
     },
@@ -25,6 +17,6 @@ export const counterSlice = createSlice({
 })
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount } = counterSlice.actions
+export const { incrementByAmount } = counterSlice.actions
 
 export default counterSlice.reducer
