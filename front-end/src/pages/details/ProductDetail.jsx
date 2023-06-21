53d8ea899568981104681533d65b8f7b6624465f
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2023-06-21 18:08:14 +0330

    Upload images to Cloudinary

--- front-end/src/pages/details/ProductDetail.jsx
+++ front-end/src/pages/details/ProductDetail.jsx
@@ -1,9 +1,30 @@
+import { Box, CircularProgress, Typography } from '@mui/material';
 import { useGetOneProductQuery } from 'Redux/ProudoctsApi';
 import React from 'react'
+import { useParams } from 'react-router-dom';
 
 const ProductDetail = () => {
-  const { data, error, isLoading } = useGetOneProductQuery(4);
+  let { id } = useParams();
+  const { data, error, isLoading } = useGetOneProductQuery(id);
 
+
+  if (isLoading) {
+    return (
+      <Box sx={{ display: "flex" }}>
+        <CircularProgress />
+      </Box>
+    );
+  }
+  if (error) {
+    return (
+      <Box sx={{ display: "flex" }}>
+        <Typography variant="h1" color="error">
+          {" "}
+          ERROR{" "}
+        </Typography>
+      </Box>
+    );
+  }
 
 
   if(data){
