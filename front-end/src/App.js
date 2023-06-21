ab7489b47668f659a9c999d87ab5d72f97a35a26
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2023-06-21 16:16:01 +0330

    react-router-dom (dynamic links)

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -12,13 +12,15 @@ import {
 
 import NotFound from "./pages/NotFound";
 import Cart from "pages/cart/Cart";
+import Productdetails from "pages/details/Productdetails";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Root />}>
       <Route index element={<Home />} />
       <Route path="cart" element={<Cart />} />
-
+      <Route path="./Productdetails:id" element={< Productdetails />} />
+ 
 
       <Route path="*" element={<NotFound />} />
 
